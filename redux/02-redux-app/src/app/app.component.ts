589ea0b5cd201df861cd15750b65d0d20d9fae74
unknown <luis.herrera@towasoftware.com>
2019-01-11 00:18:40 -0600

    aplication ngrx

---
+++ redux/02-redux-app/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Store, Action } from '@ngrx/store';
+import { IncrementarAction, DecrementarAction } from './contador/contador.actions';
+import { AppState } from './contador.reducers';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ contador: number;
+
+
+ constructor( private store: Store<AppState>){
+  //  this.contador = 10;
+
+  this.store.select('contador').subscribe(contador => {
+    this.contador = contador;
+  })
+ }
+
+ incrementar(){
+  //  this.contador ++;
+const accion = new IncrementarAction();
+
+  this.store.dispatch(accion)
+ }
+
+ decrementar(){
+  // this.contador --
+  const accion = new DecrementarAction();
+   
+     this.store.dispatch(accion)
+}
+}
---
+++ redux/02-redux-app/src/app/contador/contador.reducer.ts
@@ -0,0 +1,23 @@
+import { Action } from '@ngrx/store';
+import { INCREMENTAR, DECREMENTAR, MULTIPLICAR, actions, DIVIDIR } from './contador.actions';
+
+export function contadorReducer(state: number = 10, action: actions) {
+
+    switch (action.type) {
+        case INCREMENTAR:
+            return state + 1;
+
+
+        case DECREMENTAR:
+            return state - 1;
+
+        case MULTIPLICAR:
+            return state * action.payload;
+
+            case DIVIDIR:
+            return state / action.payload;    
+
+        default:
+            return state;
+    }
+}
